67579cf31166500fcaac5f9651a14e06df82b6f3
UtechP-40 <pradeep2420pradeep@gmail.com>
2024-08-21 19:41:29 +0530

    JS variables Declared

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const typingText = document.querySelector(".typing-text p"),
+inpField = document.querySelector(".wrapper .input-field"),
+tryAgainBtn = document.querySelector(".content button"),
+timeTag = document.querySelector(".time span b"),
+mistakeTag = document.querySelector(".mistake span"),
+wpmTag = document.querySelector(".wpm span"),
+cpmTag = document.querySelector(".cpm span");
